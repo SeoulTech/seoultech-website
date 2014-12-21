c1f46648f4a4b1fcef9b7ae5f78aa7fbb099bdd9
Alexander Bezzubov <abezzubov@nflabs.com>
2014-12-21 19:49:51 +0900

    gh-pages updated to last generated HTML

--- ui/components/Static/main.js
+++
@@ -1,8 +0,0 @@
-var React = require('react/addons')
-var $ = React.DOM
-
-module.exports = React.createClass({
-  render: function() {
-    return $.div({dangerouslySetInnerHTML: this.props})
-  }
-})
